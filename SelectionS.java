2e8a9932a167815782dec942b4f4c4558f19cdf9
Moses Yiblet <yibletm@gmail.com>
2026-05-11 22:31:40 -0400

    first commit

---
+++ SelectionS.java
@@ -0,0 +1,19 @@
+
+
+public class SelectionS{
+
+
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        for(int i=0; i<5; i++)
+        {
+            arr[i] = (int)(Math.random()*20)-10;
+            System.out.println(arr[i]);
+        }
+    }
+
+
+
+
+
+}
