40e004949041f26172f67179e3d4eb7dcce1d131
Moses Yiblet <yibletm@gmail.com>
2026-05-21 11:02:49 -0400

    Removed spares

--- BubbleS.java
+++ BubbleS.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
 public class BubbleS{
 
 public static void main(String args[])
 {
-
+    
+Scanner input = new Scanner(System.in);
 int[] arr = new int[5];
-arr[0] = 3;
-arr[1] = 4;
-arr[2] = 1;
-arr[3] = 5;
-arr[4] = 2;
+arr[0] = input.nextInt();  //3
+arr[1] = input.nextInt();  //4
+arr[2] = input.nextInt();  //1
+arr[3] = input.nextInt();  //5
+arr[4] = input.nextInt();  //2
 int curr = 0;
 //Incoming best case O(N), other case O(N^2)
 for(int i=0; i<5; i++)
@@ -18,11 +20,11 @@ for(int i=0; i<5; i++)
         if(curr < arr[j])
         {
             curr = arr[j];
-            System.err.println("Curr change");
+            System.out.println("Curr change");
         }
         else
         {
-            System.err.println("Shift");
+            System.out.println("Shift");
             arr[j-1] = arr[j];
             arr[j] = curr;
         }
